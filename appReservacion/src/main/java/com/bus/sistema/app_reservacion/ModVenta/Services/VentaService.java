ee179cd516ad3a55bfc6dbce735307172b131697
Jose Maldonado <josephcand@gmail.com>
2019-04-03 04:43:02 -0500

    revision

--- appReservacion/src/main/java/com/bus/sistema/app_reservacion/ModVenta/Services/VentaService.java
+++
@@ -1,7 +0,0 @@
-package com.bus.sistema.app_reservacion.ModVenta.Services;
-
-import com.bus.sistema.app_reservacion.ModSeguridad.Services.GenericService;
-import com.bus.sistema.app_reservacion.ModVenta.Domain.Venta;
-
-public interface VentaService extends GenericService<Venta> {
-}
